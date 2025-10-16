f7bec96d7ed6e4cb86e6efd7ebe22d133f7b50b5
L-Dramatic <3062059635@qq.com>
2025-10-17 03:43:21 +0800

    初始化

---
+++ backend/src/test/java/com/software/contract_system/ContractSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.contract_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
